e8c08fa8aeb35597b853693736123a6694fd2afe
GG-Queen <128638786+GG-Queen@users.noreply.github.com>
2023-05-03 13:00:53 -0700

    Update index.js

--- encryptor/index.js
+++ encryptor/index.js
@@ -1,2 +1,10 @@
 console.log('The encryptor js file loaded');
 
+const letteres = 'abcdefghijklmnopqrstuvwxyz';
+const encrypt_key = 'bcdefghijklmnopqrstuvwxyza';
+
+console.log(letteres[0]); // a
+
+for (let i = 0; i < letteres.length;; it++) {
+  console.lo
+
